6e9549453b8d9e092dab9de82b297f1bc608b90f
Lucas Farias <lucasfnf@gmail.com>
2025-04-26 21:07:33 -0300

    Reproduction attempt for bevyengine/bevy#1422 at 0.16

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,40 @@
 use bevy::{
     DefaultPlugins,
-    app::App,
+    app::{App, Startup, Update},
     asset::AssetServer,
-    prelude::{Camera2dBundle, Commands, Query, Res, Transform},
-    sprite::SpriteBundle,
+    prelude::{Camera2d, Commands, Query, Res, Transform},
+    sprite::Sprite,
     time::Time,
 };
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_startup_system(setup)
-        .add_system(move_bird)
+        .add_systems(Startup, setup)
+        .add_systems(Update, move_bird)
         .run();
 }
 
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
-    commands.spawn(Camera2dBundle::default());
+    commands.spawn(Camera2d);
 
-    commands.spawn(SpriteBundle {
-        texture: asset_server.load("bevy_bird_dark.png"),
-        transform: Transform::from_xyz(0., 100., 0.),
-        ..Default::default()
-    });
+    commands.spawn((
+        Transform::from_xyz(0., 100., 0.),
+        Sprite {
+            image: asset_server.load("bevy_bird_dark.png"),
+            ..Default::default()
+        },
+    ));
+    commands.spawn((
+        Transform::from_xyz(0., -100., 0.),
+        Sprite {
+            image: asset_server.load("bevy_bird_dark.png"),
+            ..Default::default()
+        },
+    ));
 
-    commands.spawn(SpriteBundle {
-        texture: asset_server.load("bevy_bird_dark.png"),
-        transform: Transform::from_xyz(0., -100., f32::EPSILON),
-        ..Default::default()
-    });
-
-    commands.spawn(SpriteBundle {
-        texture: asset_server.load("array_texture.png"),
+    commands.spawn(Sprite {
+        image: asset_server.load("array_texture.png"),
         ..Default::default()
     });
 }
@@ -39,7 +42,7 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
 fn move_bird(mut transforms: Query<&mut Transform>, time: Res<Time>) {
     for mut transform in transforms.iter_mut() {
         if transform.translation.y != 0. {
-            transform.translation.z = time.elapsed_seconds().sin();
+            transform.translation.z = time.elapsed_secs().sin();
         }
     }
 }
