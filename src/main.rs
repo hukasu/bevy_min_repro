ebba88c99b71a9aa3fc11744a0c5b5896a350288
Lucas Farias <lucasfnf@gmail.com>
2025-04-26 22:08:20 -0300

    Reproduction attempt for bevyengine/bevy#1693 at 0.16

--- src/main.rs
+++ src/main.rs
@@ -1,61 +1,31 @@
-use std::time::Duration;
-
 use bevy::{
-    DefaultPlugins,
-    app::{App, Startup, Update},
-    asset::AssetServer,
-    prelude::{Camera2d, Commands, IntoScheduleConfigs, Local, Query, Res, Transform, With},
-    sprite::Sprite,
-    time::common_conditions::on_timer,
-    window::{
-        MonitorSelection, PrimaryWindow, VideoModeSelection, Window, WindowMode, WindowResolution,
-    },
+    ecs::schedule::{LogLevel, ScheduleBuildSettings},
+    prelude::*,
 };
+
+#[derive(Debug, Default, Clone, Eq, PartialEq, Hash, States)]
+enum AppState {
+    #[default]
+    Menu,
+    InGame,
+}
+
+#[derive(Resource, Default)]
+struct Data;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_systems(Startup, setup)
-        .add_systems(
-            Update,
-            toggle_fullscreen.run_if(on_timer(Duration::from_secs(4))),
-        )
+        .init_state::<AppState>()
+        .init_resource::<Data>()
+        .configure_schedules(ScheduleBuildSettings {
+            ambiguity_detection: LogLevel::Error,
+            ..Default::default()
+        })
+        .add_systems(Update, menu.run_if(in_state(AppState::Menu)))
+        .add_systems(Update, game.run_if(in_state(AppState::InGame)))
         .run();
 }
 
-fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
-    commands.spawn(Camera2d);
-
-    commands.spawn((
-        Transform::from_xyz(0., 100., 0.),
-        Sprite {
-            image: asset_server.load("bevy_bird_dark.png"),
-            ..Default::default()
-        },
-    ));
-    commands.spawn((
-        Transform::from_xyz(0., -100., f32::EPSILON),
-        Sprite {
-            image: asset_server.load("bevy_bird_dark.png"),
-            ..Default::default()
-        },
-    ));
-
-    commands.spawn(Sprite {
-        image: asset_server.load("array_texture.png"),
-        ..Default::default()
-    });
-}
-
-fn toggle_fullscreen(mut state: Local<bool>, mut windows: Query<&mut Window, With<PrimaryWindow>>) {
-    let mut window = windows.single_mut().unwrap();
-    if *state {
-        window.resolution = WindowResolution::new(800., 640.);
-        window.mode = WindowMode::Windowed;
-    } else {
-        window.resolution = WindowResolution::new(1920., 1080.);
-        window.mode =
-            WindowMode::Fullscreen(MonitorSelection::Primary, VideoModeSelection::Current);
-    }
-    *state = !*state;
-}
+fn menu(mut _data: ResMut<Data>) {}
+fn game(mut _data: ResMut<Data>) {}
